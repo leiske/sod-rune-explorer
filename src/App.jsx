d092e97d4c88c37fe1e32ab4e26ece98d1f02fe9
Colby <colby.leiske@gmail.com>
2023-11-04 13:55:33 -0700

    add druid and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,68 @@
 import './App.css'
-import {runes} from '../runes/hunter.json';
+import { hunter } from '../runes/hunter.json';
+import { druid } from '../runes/druid.json';
 import { Tooltip } from './runeTooltip/tooltip';
+import { useState } from 'react';
 
 function App() {
-    console.log({ runes });
+  const classes = [
+      hunter,
+      druid,
+  ];
 
+  const params = new URLSearchParams(window.location.search);
+  const selectedClassName = params.get("curr");
+
+  function setSelectedClass(className) {
+    let newClassName = className;
+    if (!className || !classes.map(({ name }) => name).includes(className)) {
+      newClassName = 'hunter';
+    }
+
+    if (className === selectedClassName) {
+      // already on this one, don't redirect
+      return;
+    }
+
+    params.set('curr', newClassName);
+
+    const url = new URL(window.location.href);
+    url.search = params.toString();
+
+    window.location.assign(url.href);
+  }
+
+  // just validate and it won't redirect if it's a good class name
+  setSelectedClass(selectedClassName);
+
+  const selectedClass = classes.find(({ name }) => name === selectedClassName);
   return (
-    <div className="rune-category-container">
-        <div className="rune-category">
-            <h1>Chest Runes</h1>
-            {runes.chest.map(rune => <Tooltip rune={rune} />)}
+    <>
+        <div className="class-nav-container">
+            {classes.map((sodClass) =>
+                    <img 
+                        src={sodClass.iconSrc} 
+                        key={sodClass.iconSrc} 
+                        onClick={() => setSelectedClass(sodClass.name)}
+                        className={`class-icon ${sodClass.iconSrc === selectedClass.iconSrc ? 'class-icon-selected' : ''}`}
+                    />)}
         </div>
-        <div className="rune-category">
-            <h1>Gloves Runes</h1>
-            {runes.glove.map(rune => <Tooltip rune={rune} />)}
+
+        <div className="rune-category-container">
+            <div className="rune-category">
+                <h1>Chest Runes</h1>
+                {selectedClass.runes.chest.map(rune => <Tooltip key={rune.name} rune={rune} />)}
+            </div>
+            <div className="rune-category">
+                <h1>Gloves Runes</h1>
+                {selectedClass.runes.glove.map(rune => <Tooltip key={rune.name} rune={rune} />)}
+            </div>
+            <div className="rune-category">
+                <h1>Leg Runes</h1>
+                {selectedClass.runes.leg.map(rune => <Tooltip key={rune.name} rune={rune} />)}
+            </div>
         </div>
-        <div className="rune-category">
-            <h1>Leg Runes</h1>
-            {runes.leg.map(rune => <Tooltip rune={rune} />)}
-        </div>
-    </div>
+    </>
   )
 }
 
--- src/runeTooltip/tooltip.jsx
+++ src/runeTooltip/tooltip.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export function Tooltip({ rune }) {
-    const { name, desc, icon } = rune;
+    const { name, desc, icon, bullets } = rune;
 
     return (
         <div className="tooltip">
             <h3 className="tooltip-name">{ name }</h3>
             <img className="tooltip-icon" src={icon}/>
             <p className="tooltip-desc">{ desc }</p>
+            {bullets && bullets.map(bullet => <p className="tooltip-bullet" key={bullet}>{bullet}</p>)}
         </div>
     );
 }
